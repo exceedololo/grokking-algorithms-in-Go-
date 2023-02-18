dec672470a5d24674fa38e7ffa889f9f34bfe0ed
exceedololo <exceedololo@outlook.com>
2023-02-18 23:23:58 +0300

    searh in breadth

---
+++ BreadthFirstSearch/breadthFirstSearch.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+var graph = map[string]map[string]bool{
+	"you": {
+		"alice":  true,
+		"bob":    true,
+		"claire": true,
+	},
+	"bob": {
+		"anuj":  true,
+		"peggy": true,
+	},
+	"alice": {
+		"peggy": true,
+	},
+	"claire": {
+		"thom":  true,
+		"jonny": true,
+	},
+	"anuj":  {},
+	"peggy": {},
+	"thom":  {},
+	"jonny": {},
+}
+
+func personIsSeller(name string) bool {
+	return name[len(name)-1] == 'm'
+}
+
+func search(name string) bool {
+	searchQueue := list.New()
+	for neighbor := range graph[name] {
+		searchQueue.PushBack(neighbor)
+	}
+	searched := make(map[string]bool)
+	searched[name] = true
+	for searchQueue.Len() > 0 {
+		person := searchQueue.Remove(searchQueue.Front()).(string)
+		if !searched[person] {
+			if personIsSeller(person) {
+				fmt.Println(person + " is a mango seller!")
+				return true
+			} else {
+				for neighbor := range graph[person] {
+					searchQueue.PushBack(neighbor)
+				}
+				searched[person] = true
+			}
+		}
+	}
+	return false
+}
+
+func main() {
+	search("you")
+}
